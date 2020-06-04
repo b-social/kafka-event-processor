a91af23a8eb9ca3ac330885a6e4406465c94ee1a
Alex Parlett <alexparlett@gmail.com>
2020-06-04 17:12:38 +0100

    Allow kafka subsystem to be disabled
    Authors: @alexparlett @danielzurawski

--- src/kafka_event_processor/kafka/system.clj
+++ src/kafka_event_processor/kafka/system.clj
@@ -5,7 +5,8 @@
     [configurati.core :as conf]
 
     [kafka-event-processor.kafka.component
-     :as kafka]))
+     :as kafka]
+    [kafka-event-processor.utils.logging :as log]))
 
 (defn new-system
   "Creates a new kafka consumer client.
@@ -28,18 +29,24 @@
   ([configuration-overrides]
    (new-system configuration-overrides {}))
   ([configuration-overrides
-    {:keys [kafka kafka-configuration configuration-prefix]
+    {:keys [kafka kafka-configuration configuration-prefix kafka-enabled]
      :or   {kafka                :kafka
             kafka-configuration  :kafka-configuration
+            kafka-enabled        :kafka-enabled?
             configuration-prefix :service}}]
+   (let [kafka-enabled?
+         (get configuration-overrides kafka-enabled true)]
+     (log/log-info
+       {kafka-enabled kafka-enabled?}
+       "Kafka enabled?")
+     (when kafka-enabled?
+       (component/system-map
+         kafka-configuration
+         (conf/resolve
+           (:kafka configuration-overrides
+             (kafka/kafka-configuration configuration-prefix)))
 
-   (component/system-map
-     kafka-configuration
-     (conf/resolve
-       (:kafka configuration-overrides
-         (kafka/kafka-configuration configuration-prefix)))
-
-     kafka
-     (component/using
-       (kafka/new-kafka)
-       {:configuration kafka-configuration}))))
+         kafka
+         (component/using
+           (kafka/new-kafka)
+           {:configuration kafka-configuration}))))))
