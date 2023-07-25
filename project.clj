06613d4ea9630341385818b2d34e6b25dd12042f
Andrey Fadeev <andfadeev@gmail.com>
2023-07-25 12:21:37 +0100

    [TECH] wrap event processing with open telemetry trace (#3)
    Authors: @andfadeev @Shilcof

--- project.clj
+++ project.clj
@@ -13,7 +13,8 @@
                  [org.apache.kafka/kafka-clients "2.3.0"]
                  [cambium/cambium.core "0.9.3"]
                  [cambium/cambium.codec-cheshire "0.9.3"]
-                 [org.clojure/java.jdbc "0.7.11"]]
+                 [org.clojure/java.jdbc "0.7.11"]
+                 [com.datadoghq/dd-trace-ot "1.17.0"]]
 
   :plugins [[lein-cloverage "1.1.2"]
             [lein-shell "0.5.0"]
